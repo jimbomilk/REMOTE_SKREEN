3947211c7d2746cffa41972ef1cb1e08a9318d54
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-12-28 23:54:15 +0100

    ok

--- src/services/keepAliveService.ts
+++ src/services/keepAliveService.ts
@@ -15,7 +15,7 @@ export class keepAliveService {
 
   keepAlive(location: string): Observable<boolean> {
     return this.http
-      .get(this.appConfig.getAPIUrl() + `keepAlive?locationId=${location}&screenId=1`)
+      .get(this.appConfig.getAPIUrl() + `keepAlive?locationId=${location}&screenId=`+this.appConfig.getEnv('ip'))
       .map( res => {
         return true;
       });
